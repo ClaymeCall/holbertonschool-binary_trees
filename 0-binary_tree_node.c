11860a6ef343aea68ca6b775f5319988a8219e7c
Clement <9397@holbertonstudents.com>
2024-08-05 15:08:46 +0200

    Fixed betty coding styles errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node : add a new node to a binary tree
- * @parent: pointer to the parent
- * @value: value to be written in new node
- * 
- * Return: pointer to the new node
+ * binary_tree_node : Add a new node to a binary tree.
+ * @parent: Pointer to the parent.
+ * @value: Value to be written in new node.
+ *
+ * Return: pointer to the new node.
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if(new_node == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = value;
