845b30adc2e7a0223193c96e89ea238deb1f9a2f
Clement Callejon <9397@holbertonstudents.com>
2024-08-06 01:26:51 +0200

    Fixed logic to count correct height instead of 1 too high

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,14 +10,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (!tree)
-		return (0);
-	
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
 
 	if (left_height > right_height)
-		return(left_height + 1);
+		return(left_height);
 
-	return(right_height + 1);
+	return(right_height);
 }
