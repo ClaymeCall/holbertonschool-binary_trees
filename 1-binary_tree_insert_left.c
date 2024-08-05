22b3a1874abc79ed58de8012793892350c3edc21
Clement Callejon <9397@holbertonstudents.com>
2024-08-05 20:31:02 +0200

    Refactored function to use task 0 function as it's suggested with compilation command

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,23 +9,25 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
 
-	if (new_node == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	new_node->n = value;
+	/* Creating a new node and checking success*/
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
 
 	/* Handling parent already having a left child case */
 	if (parent->left != NULL)
 	{
-		new_node->left = parent->left;
 		parent->left->parent = new_node;
+		new_node->left = parent->left;
 	}
 
-	/* Default case */
+	/* Inserting the new node in the tree */
 	parent->left = new_node;
-	new_node->parent = parent;
 
 		return (new_node);
 }
