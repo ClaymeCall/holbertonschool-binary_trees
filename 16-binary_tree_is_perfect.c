ddbf958c7d825795971be50fad1f1e8d8e80ec68
Clement Callejon <9397@holbertonstudents.com>
2024-08-06 18:33:44 +0200

    Removed trailing white spaces

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,10 +9,10 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
-	
+
 	if (tree == NULL)
 		return (0);
-	
+
 	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
 	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
 
